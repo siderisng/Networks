990ab8ccc7ed039a14eb6ccea19d9edb40938b6a
Fotis <fotanftm@gmail.com>
2015-01-06 19:11:44 +0200

    Nothing more to say

--- Server.py
+++ Server.py
@@ -1,16 +1,19 @@
-import socket
-import sys
-from thread import *
-listConns = list ()
-listFilesConn= list()
-listFiles = list()
-listAllConns = list()
+import socket #for sockets
+import sys  #for exit
+from thread import * #for thread creation
+
+listConns = list () # list of sockets used for chat
+listFilesConn= list() # list of sockets used for chat and files associated with them
+listFiles = list() #list of files
+listAllConns = list() #list of chat and file exchange sockets
 
 
-HOST = ''   # Symbolic name meaning all available interfaces
-PORT = 8887# Arbitrary non-privileged port
+HOST = ''    # Symbolic name meaning all available interfaces
+PORT = 8888  # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
+ 
+ 
  
 #Bind socket to local host and port
 try:
@@ -21,6 +24,8 @@ except socket.error , msg:
      
 print 'Socket bind complete'
  
+ 
+ 
 #Start listening on socket
 s.listen(10)
 print 'Socket now listening'
@@ -29,32 +34,39 @@ print 'Socket now listening'
 #function for sharing files 
 def clientExchange(conn, connToSend, nofFile ):
     
+    #find socket for file exchange associated with socket that has file
     for anyConn in listAllConns:
         if anyConn[0] == connToSend:
             
             connRcv = anyConn[1]
             break
   	
-    msg = "wFILE" + str(conn)
+  	#Sent that you want a file
+    msg = "wFILE"
     connToSend.send (msg)
     
+    #wait for reply
     data = connRcv.recv(1024)
+    #if it asks you for the name of file respond
     if data == "NAME":
         connRcv.send (nofFile)	
     
+    #if it's ok receive the size of file 
     data = connRcv.recv(1024)
+    
     if data != "NOT":    
-        size = int (data)
-        print size
         
+        size = int (data)
+        #send it to client that wants the file
         msg = str (size)       
         conn.send (msg)
         
+        #start file transfer
         l = connRcv.recv(size)
-      
+        
         conn.send (l)
         print "File Sent"
-        
+    #else send client error message    
     else:
         msg = "NOT"
         conn.send (msg)
@@ -62,13 +74,13 @@ def clientExchange(conn, connToSend, nofFile ):
         
 #Function for handling connections. This will be used to create threads
 def clientthread(conn ):
-    #Sending message to connected client
-    conn.send('Welcome to the server.\nType Chat: <msg> to share a message with other clients\nType nFile <name of file> for uploading file.\nType tFile <name of file> for downloading file. \nType ShowList for showing the current list of files available\nType Q or q to exit') 
+    #Sending message to connected client/ Instruction for using server
+    conn.send('Welcome to the server.\nType Chat: <msg> to share a message with other clients\nType nFile< ><name of file> for uploading file.\nType tFile< ><name of file> for downloading file. \nType ShowList for showing the current list of files available\nType Q or q to exit') 
      
     #infinite loop so that function do not terminate and thread do not end.
     while True:
 		
-        flag = 0 
+        flag = 0 #used for checking if file exists
         
         #Receiving from client
         
@@ -80,7 +92,7 @@ def clientthread(conn ):
 		
 		#Chat
         elif data[0:5] == "Chat:":
-			
+			#Send all except this client
             for sendTo in listConns:
                 if conn == sendTo:
                     continue 
@@ -89,14 +101,14 @@ def clientthread(conn ):
         #upload        
         elif data[0:5] == "nFile":
 			
+			#read name of file and add it
             tupleFiles = (data[6:],conn)
             listFilesConn.append (tupleFiles) 
-            listFiles.append (data[6:]+"\n")
+            listFiles.append (data[6:]+", ")
+            #send list of files
             for sendTo in listConns:
                 
-                if conn == sendTo:
-                    continue 
-                sendTo.send ("List of files:\n")
+                sendTo.send ("List of files:  ")
                 for fileName in listFiles:
                     
                     sendTo.sendall(fileName)
@@ -104,34 +116,34 @@ def clientthread(conn ):
         #ask permission to take file
         elif data[0:5] == "tFile":
             
+            #find if file exists
             for aFile in listFilesConn:
                 
                 if data[6:] == aFile[0]:
 					
                      
                     if aFile[1] == conn:
-                        print "You already have this file, don't be silly"
+                        conn.send ("You already have this file, don't be silly")
                         flag =1
                         continue
                     else:
+                        #if it does call clientExchange function
                         connToSend = aFile[1]
                         nofFile = data[6:]
+                        #Send client to get ready
                         conn.send ("GET READY")
                         start_new_thread (clientExchange, (conn,connToSend,nofFile) )
                         flag =1
                         
             if flag == 0:
-                conn.send ("No such File uploaded")
+                conn.send ("Sorry no such File uploaded.  Maybe the client log out")
                 
             else:
                 flag=0
        
-        elif data[0:2] == "OK":
-            toSend = data[3:]     
-            print data
-       
        #show file list
         elif data [0:] == "ShowList":
+            #sends list back to the client that asks for it
             sendTo.send ("List of files:")
             for fileName in listFiles:
                     
@@ -139,42 +151,60 @@ def clientthread(conn ):
                     
         #QUIT
         elif (data  == "Q" or data =='q'):
-			conn.send ("Time to die")
-			break
+            
+            
+            for deleteCon in listAllConns:
+                if deleteCon[0] == conn:
+                    connDel = deleteCon[1]
+                    listAllConns.remove (deleteCon)
+                    break
+				
+            listConns.remove (conn)
+            for toDel in listFilesConn: 
+                if toDel[1]== conn:
+                    listFilesConn.remove(toDel)
+                    
+            break
 		
        #not a valid command             
         else:
-            conn.send('Syntax Error\nType Chat: <msg> to share a message with other clients\nType nFile <name of file> for uploading file.\nType tFile <name of file> for downloading file. \nType ShowList for showing the current list of files available\nType Q or q to exit')
+            conn.send('Syntax Error\nType Chat: <msg> to share a message with other clients\nType nFile< ><name of file> for uploading file.\nType tFile< ><name of file> for downloading file. \nType ShowList for showing the current list of files available\nType Q or q to exit')
         
     #came out of loop
+    conn.send ("Time to die")
+    print "Closing socket " + str(conn)
     conn.close()
- 
+    connDel.close()
+    
 #now keep talking with the client
 while 1:
     #wait to accept a connection - blocking call
     conn, addr = s.accept()
+    
     
     print 'Connected with ' + addr[0] + ':' + str(addr[1]) 
     
     data = conn.recv(1024)
     reply = data
     
+    #iif it's a chat socket add it to the appropriate list and wait for the file socket to connect it
     if reply == "CSHOC":
         listConns.append(conn)
         
         print 'List of connections:'
         print listConns 
-        toLink = str (conn)
-        conn.send (toLink)
+        toLink = str (conn) #used later for finding the right socket
+        conn.send (toLink) #sends back socket name
         
+    #if it's a file socket    
     elif reply == "FSHOC":
-        
+        #wait to aquire the name of the chat socket to link it with
         data = conn.recv(1024)
-        
+        #find its pair         
         for toLink in listConns:
             
             toCMP = str (toLink)
-           
+            #if you find it link them and start clientthread function     
             if toCMP == data: 
                 
                 tupleLinked = (toLink, conn)
--- client.py
+++ client.py
@@ -1,85 +1,99 @@
 import socket   #for sockets
 import sys  #for exit
-import os.path #for file
+import os.path #for file handling
 
-from thread import *
-i = 0
+from thread import *  #for thread handling
 
+#this function sends a requested file to server
 def giveFile():
     
+    #if it is given a file request it asks server which one
     msg = "NAME"
     sendSoc.sendto(msg , (host, port))   
    
+    #acquire name of file   
     d = sendSoc.recvfrom(1024)
-  
     fname = d[0]   
     
+    #check if there is such a file
     if os.path.isfile(fname) == True: 
+        
+        #if it does exist find the size of it
         size = os.path.getsize (fname)
         
+        #send message to server with the size of it
         msg = str (size)
         sendSoc.sendto(msg , (host, port))
         
-        sFile=open (fname, "rb") 
-  
-        l = sFile.read(1024)
-        
-        while (l):
-            sendSoc.send(l)
-            l = sFile.read(1024)
-        print "Done"
+        #open file
+        sFile=open (fname, "rb")
+        #read all and send
+        l = sFile.read(size)
+        sendSoc.send(l)
+        #close file and print appropriate message   
         sFile.close ()
+        print "Just Sent a File"
+    
+    #if file doesn't exist send negative message
     else:
+		
         msg = "NOT"
         sendSoc.sendto(msg , (host, port))
 
 
+#used for getting info from server
 def ListenToTheServer():
     i=0
     while(1):
-
-        #Set the whole string
         
-        # receive data from client (data, addr)
+        # receive data from server
         d = s.recvfrom(1024)
         reply = d[0]
             
-      
+        #if the message is time to die then it's time to end the programm
         if reply == "Time to die":
-            break
+            print "BB!!!"
+            sys.exit()
+        
+        #if there is a file request start giveFile function
         elif reply[:5]=="wFILE":
             start_new_thread(giveFile ,())
+        
+        #if GET READY is the answer then client must wait till he gets back the file he asked for
         elif reply == "GET READY":
             print "Starting Sending procedures. Please do not send anything till it's over"
-            
+            #get size of file from server
             d = s.recvfrom(1024)
             reply = d[0]   
             
-			
+			#if everything's ok get file
             if reply != "NOT":
-               
+                
                 size = int(reply) 
-                nfile = open ("nFile_"+ str(i),'wb')
+                #create new file with unique name
+                nfile = open ("nFile_"+ str(i)+ str(s),'wb')
                 i= i+1
-                print size
+                
+                #receive file
                 l = s.recv(size)
+                #write it to file then close it
                 nfile.write(l)
                 nfile.close ()
                    
-                    
                 print "OK... FILE OBTAINED"
-                
+ 
+            #else say that something went wrong    
             else:
                 print "Something went wrong. Please try again. It's possible that client doesn't have file anymore.."
                 
-                
+        #if answer does not match any other just print the reply        
         else: 
-            print reply 		
+            print reply	
             
             
             
-host = "localhost"
-port = 8887
+host = "localhost" 
+port = 8888
 
 #socket for sending file   
 
@@ -94,7 +108,7 @@ except socket.error, msg:
 print 'Socket Created'
 
 
-#main socket
+#socket chat socket
 try:
     #create an AF_INET, STREAM socket (TCP)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -106,7 +120,7 @@ print 'Socket Created'
  
 #end of socket creation
 
-
+#find ip of host
 try:
     remote_ip = socket.gethostbyname( host )
  
@@ -118,36 +132,34 @@ except socket.gaierror:
 print 'Ip address of ' + host + ' is ' + remote_ip
  
 
-#Connect to remote server
+#Connect chat socket to remote server
 s.connect((remote_ip , port))
-
-
- 
 print 'Socket for Chat Connected to ' + host + ' on ip ' + remote_ip
 
+#send type of socket
 msg = "CSHOC"
 s.sendto(msg , (host, port))
 
+#acquire name of socket in server
 d = s.recvfrom(1024)
 reply = d[0]
-print reply
 
+
+#Connect file exchange socket to remote server
 sendSoc.connect((remote_ip , port))
-
 print 'Socket for file exchange connected to ' + host + ' on ip ' + remote_ip
 
+#send type of socket
 msg = "FSHOC"
 sendSoc.sendto(msg , (host, port))
+#send socket to link it with
 sendSoc.sendto(reply , (host, port))
 
 
-d = s.recvfrom(1024)
-reply = d[0]
-addr = d[1]
-print 'Server reply : ' + reply
-
+#start new thread for listening to the server
 start_new_thread(ListenToTheServer ,())
 
+#this is used for sending messages to the server
 while(1) :
        
     try:
